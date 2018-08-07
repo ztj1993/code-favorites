a535a8d70c277584b7c1a2c4f4ac573daa389939
zhangtianjie <715751607@qq.com>
2018-08-07 12:44:27 +0800

    增加了 CentOS Mirror 脚本

---
+++ Shell_CentOS/Mirror.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+###############
+# Name: CentOS 设置本地映像
+# Author: ZhangTianJie
+# Email: ztj1993@gmail.com
+# Use: curl -sSL https://raw.githubusercontent.com/ztj1993/CodeFavorites/master/Shell_CentOS/Mirror.sh | sh
+###############
+
+### 设置变量
+OS_VERSION=$(cat /etc/os-release | grep VERSION_ID | cut -d '"' -f 2 |  awk -F'.' '{ print $1 }')
+DateTime=`date +%Y-%m-%d-%H-%M-%S`
+
+### 设置镜像源
+if [ "${OS_VERSION}" == "7" ]; then
+    mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup.${DateTime}
+    curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+fi
+
+sleep 2
+
+### 生成缓存
+yum makecache
+
+sleep 2
