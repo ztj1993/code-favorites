930d5cfca153d2882751b81397e6afdb134f1903
zhangtianjie <715751607@qq.com>
2018-08-17 09:18:18 +0800

    添加了 解决 Dash Bash 问题

---
+++ Shell_Ubuntu/Block.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+###############
+# Name: 适用于 Ubuntu 的区块脚本
+# Author: ZhangTianJie
+# Email: ztj1993@gmail.com
+###############
+
+##### 解决 Dash Bash 问题 #####
+# Use: curl -sSL http://dwz.cn/m155AE9N | sed -n '11, 11p' | bash
+sudo ln -fs /bin/bash /bin/sh
