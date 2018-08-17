e45e52c07ab84141b2446a38945a9248ab18666e
zhangtianjie <715751607@qq.com>
2018-08-17 09:22:37 +0800

    优化了 AllowRootSsh.sh

--- Shell_Ubuntu/AllowRootSsh.sh
+++ Shell_Ubuntu/AllowRootSsh.sh
@@ -12,7 +12,7 @@
 [ "${RootNewPwd}" == "" ] && read RootNewPwd
 
 ### 安装必要软件
-sudo apt-get install openssh-server
+sudo apt-get install -y openssh-server
 [ $? -ne 0 ] && echo ">>>>>> Error:$(basename ${BASH_SOURCE[0]%.*}) - ($LINENO)" && exit 1
 
 ### 设置允许 Root 登陆
