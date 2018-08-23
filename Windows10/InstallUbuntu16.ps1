e1b3e043fdc97c7db8860ac9637f9a1478c3f084
zhangtianjie <715751607@qq.com>
2018-08-23 13:13:58 +0800

    修正了 InstallUbuntu16.ps1

--- Windows10/InstallUbuntu16.ps1
+++ Windows10/InstallUbuntu16.ps1
@@ -3,7 +3,7 @@
 # Name: 安装 Ubuntu 16 版本
 # Author: ZhangTianJie
 # Email: ztj1993@gmail.com
-# Use: PowerShell -Command "(Invoke-WebRequest http://dwz.cn/hqp2PM1S).content"
+# Use: PowerShell -Command "PowerShell -Command (Invoke-WebRequest http://dwz.cn/hqp2PM1S).content"
 ###############
 
 $ProgressPreference = 'SilentlyContinue'
@@ -17,6 +17,9 @@ if (-Not(Test-Path ~/Ubuntu.zip)) {
 if (-Not(Test-Path ~/Ubuntu)) {
     Expand-Archive ~/Ubuntu.zip ~/Ubuntu
 }
+
+# 清理旧的应用
+~/Ubuntu/ubuntu.exe clean
 
 # 运行应用
 ~/Ubuntu/ubuntu.exe
