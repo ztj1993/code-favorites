43c02969c943653729c2344873da460a3b5e7798
zhangtianjie <715751607@qq.com>
2018-08-23 12:50:03 +0800

    增加了 Windows10/InstallUbuntu16.ps1

---
+++ Windows10/InstallUbuntu16.ps1
@@ -0,0 +1,22 @@
+
+###############
+# Name: 安装 Ubuntu 16 版本
+# Author: ZhangTianJie
+# Email: ztj1993@gmail.com
+# Use: PowerShell -Command "(Invoke-WebRequest http://dwz.cn/hqp2PM1S).content"
+###############
+
+$ProgressPreference = 'SilentlyContinue'
+
+# 下载应用
+if (-Not(Test-Path ~/Ubuntu.zip)) {
+    Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1604 -OutFile ~/Ubuntu.zip -UseBasicParsing
+}
+
+# 解压应用
+if (-Not(Test-Path ~/Ubuntu)) {
+    Expand-Archive ~/Ubuntu.zip ~/Ubuntu
+}
+
+# 运行应用
+~/Ubuntu/ubuntu.exe
