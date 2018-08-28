ba9a6fb1fb437b5812d376c4f2832fbc88e1ad7e
zhangtianjie <715751607@qq.com>
2018-08-28 13:08:01 +0800

    优化了 InstallUbuntu16.ps1

--- Windows10/InstallUbuntu16.ps1
+++ Windows10/InstallUbuntu16.ps1
@@ -15,16 +15,19 @@ if ( [String]::IsNullOrEmpty($env:UbuntuDir))
 {
     $env:UbuntuDir = $env:USERPROFILE
 }
+echo 'Ubuntu app path: ' + $env:UbuntuDir
 
 # 设置 Ubuntu 应用包下载地址
 if ( [String]::IsNullOrEmpty($env:UbuntuUri))
 {
     $env:UbuntuUri = 'https://aka.ms/wsl-ubuntu-1604'
 }
+echo 'Ubuntu app uri: ' + $env:UbuntuUri
 
 # 下载应用
 if (-Not(Test-Path $env:UbuntuDir\Ubuntu.zip))
 {
+    echo 'Ubuntu app downloading ......'
     Invoke-WebRequest -Uri $env:UbuntuUri -OutFile $env:UbuntuDir\Ubuntu.zip -UseBasicParsing
 }
 
@@ -39,13 +42,21 @@ if ((Get-FileHash $env:UbuntuDir\Ubuntu.zip -Algorithm MD5).Hash -ne 'F59BD7D5A8
 {
     echo 'File md5 check failure'
     exit (1)
+} else {
+    echo 'Ubuntu app check ok'
 }
 
 # 解压应用
+echo 'Ubuntu app unzip ......'
 Expand-Archive $env:UbuntuDir\Ubuntu.zip $env:UbuntuDir\Ubuntu
 
 # 清理旧的应用
+echo 'Ubuntu app clean ......'
 PowerShell -Command $env:UbuntuDir\Ubuntu\ubuntu.exe clean
 
 # 运行应用
+echo 'Ubuntu app run ......'
 PowerShell -Command $env:UbuntuDir\Ubuntu\ubuntu.exe
+
+# 安装完成
+echo 'Ubuntu app install complete'
