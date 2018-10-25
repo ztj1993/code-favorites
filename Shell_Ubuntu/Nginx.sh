378268d4ce9507cffe359915c53f8038a99c492c
zhangtianjie <715751607@qq.com>
2018-10-25 14:56:51 +0800

    重构了 Nginx 安装脚本

--- Shell_Ubuntu/Nginx.sh
+++ Shell_Ubuntu/Nginx.sh
@@ -1,28 +1,22 @@
 #!/bin/bash
 
 ###############
-# Name: 安装最新版 Nginx
+# Name: 安装 Nginx
 # Author: ZhangTianJie
 # Email: ztj1993@gmail.com
 # Use: curl -sSL https://dwz.cn/o17MNmN8 > /tmp/Nginx && bash /tmp/Nginx
 ###############
 
-! which nginx &>/dev/null || exit 1
-
-nginx_sources_list=/etc/apt/sources.list.d/nginx.list
-
-wget -O /tmp/nginx_signing.key http://nginx.org/keys/nginx_signing.key
-sudo apt-key add /tmp/nginx_signing.key
-
-ubuntu_codename=`lsb_release --codename --short`
-
-cat<<-Note >/tmp/nginx-stable.sources.list
-#set nginx sources
-deb http://nginx.org/packages/ubuntu/ $ubuntu_codename nginx
-deb-src http://nginx.org/packages/ubuntu/ $ubuntu_codename nginx
-Note
-
-sudo mv /tmp/nginx.sources.list $nginx_sources_list
-
+### 设置镜像源
+NginxMirror=${NginxMirror:-"deb http://nginx.org/packages/ubuntu/ $(lsb_release -sc) nginx"}
+echo "${NginxMirror}" | sudo tee /etc/apt/sources.list.d/nginx.list
 sudo apt-get -y update
+
+### 导入密钥
+wget -qO- http://nginx.org/keys/nginx_signing.key | apt-key add -
+
+### 安装 Nginx
 sudo apt-get -y install nginx
+
+# 删除配置文件
+sudo rm -rf /etc/apt/sources.list.d/nginx.list
