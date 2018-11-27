172b1560241f1ac6367a9436544a987892aa9c78
ZhangTianJie <715751607@qq.com>
2018-11-27 19:14:04 +0800

    增加了 PhpMyAdmin.sh

---
+++ Linux/PhpMyAdmin.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+###############
+# Name: 安装 Composer
+# Author: ZhangTianJie
+# Email: ztj1993@gmail.com
+# Use: curl -sSL https://dwz.cn/RmP7RLuL > /tmp/PhpUnit && bash /tmp/PhpUnit
+###############
+
+### 定义变量
+ShellDir=$(cd $(dirname $(readlink -f ${BASH_SOURCE[0]})) && pwd)
+SoftwareVersion="4.8.3"
+SoftwareUrl="https://files.phpmyadmin.net/phpMyAdmin/${SoftwareVersion}/phpMyAdmin-${SoftwareVersion}-all-languages.tar.gz"
+SoftwarePackagePath="/tmp/phpMyAdmin-${SoftwareVersion}-all-languages.tar.gz"
+SoftwareInstallPath="${ShellDir}/phpMyAdmin"
+SoftwareConfigPath="${SoftwareInstallPath}/phpMyAdmin"
+
+### 下载文件
+sudo rm -rf ${SoftwarePackagePath}
+sudo wget -O ${SoftwarePackagePath} ${SoftwareUrl}
+
+### 解压文件
+sudo rm -rf ${SoftwareInstallPath}
+sudo mkdir -p ${SoftwareInstallPath}
+sudo tar -xf ${SoftwarePackagePath} --strip-components 1 -C ${SoftwareInstallPath}
+sudo chown -R 777 ${SoftwareInstallPath}
+
+### 配置
+echo "
+<?php
+\$cfg['LoginCookieValidity'] = 144000;
+\$cfg['AllowArbitraryServer'] = true;
+" | sudo tee ${SoftwareInstallPath}/config.inc.php
