22d347106eeb38826d4e89a40a279df99725d529
ZhangTianJie <715751607@qq.com>
2018-12-01 17:21:38 +0800

    分拆优化了 AutoSource.ps1

--- PowerShell/AutoSource.ps1
+++ PowerShell/AutoSource.ps1
@@ -5,34 +5,42 @@
 # Use: PowerShell iex(New-Object Net.WebClient).DownloadString('https://dwz.cn/mPPBKOv2')
 ###############
 
-function AutoSource($FilePath, $arguments, $LocalPath, $RemoteUri)
+function AutoContent($FilePath, $LocalPath, $RemoteUri)
 {
     # 设置默认本地路径
     if (($LocalPath -eq "") -or ($LocalPath -eq $null))
     {
-        $LocalPath = "$env:ScriptLocalPath"
+        $LocalPath = "$AutoLocalPath"
     }
     # 设置默认远程路径
     if (($RemoteUri -eq "") -or ($RemoteUri -eq $null))
     {
-        $RemoteUri = "$env:ScriptRemoteUri"
+        $RemoteUri = "$AutoRemoteUri"
     }
     # 获取脚本内容
     if (Test-Path "$LocalPath/$FilePath")
     {
-        $Script = (New-Object Net.WebClient).DownloadString("$LocalPath/$FilePath")
+        $Content = (New-Object Net.WebClient).DownloadString("$LocalPath/$FilePath")
     }
     else
     {
-        $Script = (New-Object Net.WebClient).DownloadString("$RemoteUri/$FilePath")
+        $Content = (New-Object Net.WebClient).DownloadString("$RemoteUri/$FilePath")
     }
+
+    return $Content
+}
+
+function AutoSource($FilePath, $arguments, $LocalPath, $RemoteUri)
+{
+    $ScriptContent = AutoContent $FilePath $LocalPath $RemoteUri
     # 执行脚本
     if (($arguments -eq "") -or ($arguments -eq $null))
     {
-        Invoke-Command -ScriptBlock ([ScriptBlock]::Create($Script))
+        Invoke-Command -ScriptBlock ([ScriptBlock]::Create($ScriptContent))
     }
     else
     {
-        Invoke-Command -ScriptBlock ([ScriptBlock]::Create($Script)) -ArgumentList $arguments
+        Invoke-Command -ScriptBlock ([ScriptBlock]::Create($ScriptContent)) -ArgumentList $arguments
     }
 }
+
