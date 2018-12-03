89d53aa56bf3c595ebb984c5635684cc4a6dfb25
ZhangTianJie <715751607@qq.com>
2018-12-03 16:32:48 +0800

    修正了 SettingJetBrains.ps1

--- PowerShell/SettingJetBrains.ps1
+++ PowerShell/SettingJetBrains.ps1
@@ -8,28 +8,28 @@
 if (($env:IdeaConfigFilePath -eq "") -or ($env:IdeaConfigFilePath -eq $null))
 {
     Write-Warning -Message "var <IdeaConfigFilePath> undefined."
-    return 1
+    exit
 }
 
 if (($env:IdeaConfigDirPath -eq "") -or ($env:IdeaConfigDirPath -eq $null))
 {
     Write-Warning -Message "var <IdeaConfigDirPath> undefined."
-    return 1
+    exit
 }
 
-if (-Not(Test-Path $IdeaConfigFilePath))
+if (-Not(Test-Path $env:IdeaConfigFilePath))
 {
-    Write-Warning -Message "var <IdeaConfigFilePath> does not exist, Please check: $IdeaConfigFilePath"
-    return 1
+    Write-Warning -Message "var <IdeaConfigFilePath> does not exist, Please check: $env:IdeaConfigFilePath"
+    exit
 }
 
-if (-Not(Test-Path "$IdeaConfigDirPath"))
+if (-Not(Test-Path $env:IdeaConfigDirPath))
 {
-    Write-Warning -Message "var <IdeaConfigDirPath> does not exist, Please check: $IdeaConfigDirPath"
-    return 1
+    Write-Warning -Message "var <IdeaConfigDirPath> does not exist, Please check: $env:IdeaConfigDirPath"
+    exit
 }
 
-$Content = Get-Content -Path $IdeaConfigFilePath
+$Content = Get-Content -Path $env:IdeaConfigFilePath
 $Content = $Content -replace "# idea.config.path", "idea.config.path"
 $Content = $Content -replace "# idea.system.path", "idea.system.path"
 $Content = $Content -replace "# idea.plugins.path", "idea.plugins.path"
@@ -38,4 +38,4 @@ $Content = $Content -replace "# idea.log.path", "idea.log.path"
 $Content = $Content -replace "idea.config.path=(.*)", "idea.config.path=$env:IdeaConfigDirPath/config"
 $Content = $Content -replace "idea.system.path=(.*)", "idea.system.path=$env:IdeaConfigDirPath/system"
 
-$Content | Out-File $IdeaConfigFilePath -encoding ASCII
+$Content | Out-File $env:IdeaConfigFilePath -encoding ASCII
