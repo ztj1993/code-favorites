9eeec1d99127aef22b145fc2e2e99183599a6fef
Ztj <715751607@qq.com>
2019-06-17 13:50:55 +0800

    Update VpnClient.ps1

--- PowerShell/VpnClient.ps1
+++ PowerShell/VpnClient.ps1
@@ -5,9 +5,9 @@
 # Use: PowerShell iex(New-Object Net.WebClient).DownloadString('https://dwz.cn/CxHFkLgw')
 ###############
 
-$Name = Read-Host -Prompt '请输入 VPN 名称'
-$Server = Read-Host -Prompt '请输入 VPN 服务器地址'
-$Psk = Read-Host -Prompt '请输入 VPN 预共享密钥'
+$Name = Read-Host -Prompt "请输入 VPN 名称"
+$Server = Read-Host -Prompt "请输入 VPN 服务器地址"
+$Psk = Read-Host -Prompt "请输入 VPN 预共享密钥"
 
 REG ADD HKLM\SYSTEM\CurrentControlSet\Services\PolicyAgent /v AssumeUDPEncapsulationContextOnSendRule /t REG_DWORD /d 0x2 /f
 REG ADD HKLM\SYSTEM\CurrentControlSet\Services\RasMan\Parameters /v ProhibitIpSec /t REG_DWORD /d 0x0 /f
